ab45287b561d4432fcd2cff440884dc6a25561e8
isaac <isaacgarc.dev@gmail.com>
2023-08-31 15:45:48 -0300

    Initial commit

---
+++ TheNewBoston/src/test/kotlin/com/garciamaclean/TheNewBoston/TheNewBostonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.garciamaclean.TheNewBoston
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheNewBostonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
